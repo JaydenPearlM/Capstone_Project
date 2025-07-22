66f7379d89c3d7a395e2a85f46fee4af7aa999b8
Jayden Maxwell <maxwellpearl6790@gmail.com>
2025-07-22 19:03:27 -0400

    This is the whole API, I still have six more todos on it!

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -1,10 +1,70 @@
 const Transaction = require('../models/Transaction');
+const logger = require('../config/logger');
 
-exports.getAll = async (req, res) => {
-  const txs = await Transaction.find().populate('category');
-  res.json(txs);
+// GET /api/v1/transactions
+exports.getAll = async (req, res, next) => {
+  try {
+    const list = await Transaction.find({ user: req.user.id });
+    res.json(list);
+  } catch (err) {
+    next(err);
+  }
 };
-exports.getById = async (req,res) => { /*...*/ };
-exports.create  = async (req,res) => { /*...*/ };
-exports.update  = async (req,res) => { /*...*/ };
-exports.remove  = async (req,res) => { /*...*/ };
+
+//fetching data
+
+exports.fetchFromPlaid = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.is);
+    const txns = await plaidClient.getTransactions(user.plaidAccessToken);
+    res.json(txns);    
+  } catch (err) {
+    next(err);
+  }
+}
+
+// GET /api/v1/transactions/:id
+exports.getById = async (req, res, next) => {
+  try {
+    const tx = await Transaction.findOne({ _id: req.params.id, user: req.user.id });
+    if (!tx) return res.status(404).json({ message: 'Not found' });
+    res.json(tx);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// POST /api/v1/transactions
+exports.create = async (req, res, next) => {
+  try {
+    const newTx = await Transaction.create({ ...req.body, user: req.user.id });
+    res.status(201).json(newTx);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// PUT /api/v1/transactions/:id
+exports.update = async (req, res, next) => {
+  try {
+    const updated = await Transaction.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.id },
+      req.body,
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// DELETE /api/v1/transactions/:id
+exports.remove = async (req, res, next) => {
+  try {
+    await Transaction.findOneAndDelete({ _id: req.params.id, user: req.user.id });
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
+};
+
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,6 +1,7 @@
 // server/routes/categoryRoutes.js
 const router = require('express').Router();
 const ctrl   = require('../controllers/categoryController');
+const logger = require('../config/logger');
 
 router.get('/',    ctrl.getAll);
 router.get('/:id', ctrl.getById);
@@ -8,4 +9,24 @@ router.post('/',   ctrl.create);
 router.put('/:id', ctrl.update);
 router.delete('/:id', ctrl.remove);
 
+const { body, validationResult } = require('express-validator');
+
+router.post(
+  '/',
+  [
+    body('name').isString().notEmpty(),
+    body('type').isIn(['income','expense']),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+  ctrl.create
+);
+
+
 module.exports = router;
+
